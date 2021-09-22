6a9e94de02aacc40901fb6e3fc5dfb038fe9fab0
jack <jack.kelling7@gmail.com>
2021-09-22 17:16:54 -0500

    added header section with light styling

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {useState} from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="TitleSection">
+        <h1>Pokemon Stats</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
