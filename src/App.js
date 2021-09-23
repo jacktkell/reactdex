e367248903264d4e52c595dfdd3b50ba40328da5
jack <jack.kelling7@gmail.com>
2021-09-23 13:07:42 -0500

    added api get request via axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import './App.css';
-import {useState} from 'react';
+import "./App.css";
+import { useState } from "react";
+import Axios from "axios";
 
 function App() {
+  const [pokemonName, setPokemonName] = useState("");
+
+  const searchPokemon = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response) => {
+      console.log(response);
+    })
+  }
+
   return (
     <div className="App">
       <div className="TitleSection">
         <h1>Pokemon Stats</h1>
-        <input type="text"/>
-        <button>Search Pokemon</button>
+        <input
+          type="text"
+          onChange={(e) => {
+            setPokemonName(e.target.value);
+          }}
+        />
+        <button onClick={searchPokemon}>Search Pokemon</button>
       </div>
     </div>
   );
