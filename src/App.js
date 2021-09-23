ed924850b1cbd2d829a9f4c1e854f10398c6ec58
jack <jack.kelling7@gmail.com>
2021-09-23 13:30:11 -0500

    fetch specific info from api, same info rendered on page. MVP done

--- src/App.js
+++ src/App.js
@@ -4,12 +4,33 @@ import Axios from "axios";
 
 function App() {
   const [pokemonName, setPokemonName] = useState("");
+  const [pokemonChosen, setPokemonChosen] = useState(false);
+  const [pokemon, setPokemon] = useState({
+    name: "",
+    species: "",
+    image: "",
+    hp: "",
+    attack: "",
+    defense: "",
+    type: "",
+  });
 
   const searchPokemon = () => {
-    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response) => {
-      console.log(response);
-    })
-  }
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(
+      (response) => {
+        setPokemon({
+          name: pokemonName,
+          species: response.data.species.name,
+          img: response.data.sprites.front_default,
+          hp: response.data.stats[0].base_stat,
+          attack: response.data.stats[1].base_stat,
+          defense: response.data.stats[2].base_stat,
+          type: response.data.types[0].type.name,
+        });
+        setPokemonChosen(true);
+      }
+    );
+  };
 
   return (
     <div className="App">
@@ -22,6 +43,21 @@ function App() {
           }}
         />
         <button onClick={searchPokemon}>Search Pokemon</button>
+      </div>
+      <div className="DisplaySection">
+        {!pokemonChosen ? (
+          <h1>Please choose a Pokemon</h1>
+        ) : (
+          <>
+          <h1>{pokemon.name}</h1>
+          <img src={pokemon.img}/>
+          <h3>Species: {pokemon.species}</h3>
+          <h3>Type: {pokemon.type}</h3>
+          <h4>HP: {pokemon.hp}</h4>
+          <h4>Attack: {pokemon.attack}</h4>
+          <h4>Defense: {pokemon.defense}</h4>
+          </>
+        )}
       </div>
     </div>
   );
